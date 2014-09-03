4604b8b42877f5c75c4d4d885c23aa150a049d1a
dperconti <donatop34@gmail.com>
2014-09-03 13:26:00 -0700

    Adding easy install for apps I like

---
+++ easyInstall.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: dmginstall [requirements file]"
+  exit 1
+fi
+
+getDMGs() {
+    url="$1"
+    # Generate a random file name
+	tmp_file=/tmp/`openssl rand -base64 10 | tr -dc '[:alnum:]'`.dmg
+	apps_folder='/Applications'
+ 
+	# Download file
+	echo "Downloading $url..."
+	curl -# -L -o $tmp_file $url
+ 
+	echo "Mounting image..."
+	volume=`hdiutil mount $tmp_file | tail -n1 | perl -nle '/(\/Volumes\/[^ ]+)/; print $1'`
+ 
+	# Locate .app folder and move to /Applications
+	app=`find $volume/. -name *.app -maxdepth 1 -type d -print0`
+	echo "Copying `echo $app | awk -F/ '{print $NF}'` into $apps_folder..."
+	cp -ir $app $apps_folder
+ 
+	# Unmount volume, delete temporal file
+	echo "Cleaning up..."
+	hdiutil unmount $volume -quiet
+	rm $tmp_file
+ 
+	echo "Done!"
+}
+
+
+while read line
+do
+	in=$line
+
+	IFS="'" read x whatiwant y <<< "$in"
+	if [ -n "$whatiwant" ]; then
+    	getDMGs "$whatiwant"
+	fi
+	     
+done < $1
+
+
+
+
+
